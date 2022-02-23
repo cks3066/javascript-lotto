648c00c0d2dec42ced7d11130ae10d45448a68d9
cks3066 <cks3066@naver.com>
2022-02-23 16:29:14 +0900

    refactor: 배포 관련 수정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './css/index.css';
+import './js/app.js';
 import './css/toggle.css';
-import App from './js/app';
-import LottoView from './js/ui/LottoView.js';
-App();
-
-window.addEventListener('DOMContentLoaded', () => {
-  const lottoView = new LottoView();
-  lottoView.bindEvents();
-});
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,4 @@
 import LottoView from './ui/LottoView.js';
 
-function App() {
-  window.addEventListener('DOMContentLoaded', () => {
-    const lottoView = new LottoView();
-    lottoView.bindEvents();
-  });
-}
-
-App();
-export default App;
+const lottoView = new LottoView();
+lottoView.bindEvents();
